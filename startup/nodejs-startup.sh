e871856418528343f9a582b62ac1db98a50e9c41
phurikornt <mysteriousphu@gmail.com>
2023-08-15 15:20:06 +0700

    version : 1.0.9 add Start up and Dashboard home

---
+++ startup/nodejs-startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Nodejs สำหรับ nodejs
+bash -c "source /home/agv/Desktop/code/Package-AGV2/devel/setup.bash &&
+	 cd /home/agv/Desktop/code/Package-AGV2/agv-web-v2 &&  
+	 /home/agv/.nvm/versions/node/v16.17.0/bin/node index.js
+	"
+
---
+++ startup/ros-startup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+bash -c "source /home/agv/Desktop/code/Package-AGV2/devel/setup.bash &&  
+	 roslaunch manager_robot start.launch"
